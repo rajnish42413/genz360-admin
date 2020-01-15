bbe210599dd98d040a13cae7357ca9e5d024715b
rajnish42413 <rajnish42413@gmail.com>
2020-01-15 15:26:44 +0530

    bug fix

--- resources/views/camp-influencer.blade.php
+++ resources/views/camp-influencer.blade.php
@@ -32,7 +32,7 @@
 <div class="page-header row no-gutters py-4">
     <div class="col-12 col-sm-4 text-center text-sm-left mb-0">
       <span class="text-uppercase page-subtitle">Dashboard</span>
-      <h1 class="page-title">Campaign Inluencers Overview</h1>
+      <h1 class="page-title">Campaign Influencers Overview</h1>
     </div>
   </div>
 
